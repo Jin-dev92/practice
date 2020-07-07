c7d2135985683d47b33ab3c1bbf4ec1872f9e307
Jin-devST <31203142+Jin-dev92@users.noreply.github.com>
2020-07-07 12:05:39 +0900

    Update main.js

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,7 +1,6 @@
 var http = require('http'); // 모듈 사용 선언
 var fs = require('fs');
 var url = require('url');
-
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var querydata = url.parse(_url,true).query;
@@ -9,7 +8,30 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url,true).pathname;
 
     // console.log(url.parse(_url,true).path);
-
+    function createTemplate(title,list,body) {
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+            <li><a href="./data/${title}">${title}</a></li>
+            <li><a href="./data/${title}">${title}</a></li>
+            <li><a href="./data/${title}">${title}</a></li>
+        </ol>
+        <h2>${list}</h2>
+        <p>
+            ${body}
+        </p>
+        </body>
+        </html>
+        `;
+        return template;
+    }
 
     if(_url == '/'){
       _url = '/index.html';
@@ -26,30 +48,13 @@ var app = http.createServer(function(request,response){
         //  else {
             fs.readFile(`data/${querydata.id}`,'utf8',function(err,description){
                 // var description = data;
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${querydata.id}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="1.html">HTML</a></li>
-                    <li><a href="2.html">CSS</a></li>
-                    <li><a href="3.html">JavaScript</a></li>
-                </ol>
-                <h2>${querydata.id}</h2>
-                <p>
-                    ${description}
-                </p>
-                </body>
-                </html>
-                `;
+                // console.log(querydata.id);
+                
+                var temp = createTemplate(querydata.id,querydata.id,querydata.id);
+
                 response.writeHead(200);
                 // response.end(fs.readFileSync(__dirname + _url));
-                response.end(template);
+                response.end(temp);
             }); 
         // }
     }else{
